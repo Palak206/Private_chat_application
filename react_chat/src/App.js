0f961fedd38bac48c43670c55f1cb10d0c89b06e
Palak Goyal <goyalpalak1234@gmail.com>
2023-02-22 20:05:32 +0530

    First Commit

---
+++ react_chat/src/App.js
@@ -0,0 +1,23 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '1564f079-c412-4e04-9a07-2eb585ffe662'
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+
+  );
+};
+
+export default App;
---
+++ react_chat/src/components/LoginForm.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const projectID = '1564f079-c412-4e04-9a07-2eb585ffe662';
+
+const Modal = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const authObject = { 'Project-ID': projectID, 'User-Name': username, 'User-Secret': password };
+
+        try {
+            // username / password => chatengine -> give message 
+            // then workout -> correcly(logged in)
+
+            await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+
+            window.location.reload();
+            setError('');
+        } catch (err) {
+            // error -> try with new username
+            setError('Oops, incorrect credentials.');
+        }
+    };
+
+    return (
+        <div className="wrapper">
+            <div className="form">
+                <h1 className="title">Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
+                    <div align="center">
+                        <button type="submit" className="button">
+                            <span>Start chatting</span>
+                        </button>
+                    </div>
+                </form>
+                <h1>{error}</h1>
+            </div>
+        </div>
+
+    );
+};
+
+export default Modal;
+
